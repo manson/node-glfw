a5b365ad33d59c88566d9ba90b2c68dfb7a1e806
Kenny Yuan <kaining.yuan@intel.com>
2017-08-24 15:51:25 +0800

    Enable parallel build for Windows GLFW

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 cd deps/glfw-3.0.4
 cmake -DBUILD_SHARED_LIBS=1 -DGLFW_BUILD_EXAMPLES=0 -DGLFW_BUILD_TESTS=0 -DGLFW_BUILD_DOCS=0 -DCMAKE_GENERATOR_PLATFORM=x64 .
 REM cmake -DBUILD_SHARED_LIBS=1 -G "Visual Studio 14 2015 Win64" .
-msbuild GLFW.sln /p:Configuration=Release
+msbuild GLFW.sln /p:Configuration=Release /m:4
 cd ../..
 node-gyp rebuild -j 4
