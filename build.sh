13db17d3a2c20b0df2872d42795b1ee9b1579b07
Kenny Yuan <kaining.yuan@intel.com>
2017-08-24 15:48:08 +0800

    Removing unnecessary targets of GLFW, enable parallel build on Linux

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 pushd deps/glfw-3.0.4
 
-cmake -DBUILD_SHARED_LIBS=1 .
-make
+cmake -DBUILD_SHARED_LIBS=1 -DGLFW_BUILD_EXAMPLES=0 -DGLFW_BUILD_TESTS=0 -DGLFW_BUILD_DOCS=0 .
+make -j 4
 
 popd
 
-node-gyp rebuild
+node-gyp rebuild -j 4
