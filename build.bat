57fb6ee6d5a7020feda137620f1ce001d2e40efc
Kenny Yuan <kaining.yuan@intel.com>
2017-08-24 11:16:29 +0800

    Using universal way to specify x64 build target

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 cd deps/glfw-3.0.4
-cmake -DBUILD_SHARED_LIBS=1 -G "Visual Studio 14 2015 Win64" .
+cmake -DBUILD_SHARED_LIBS=1 -DCMAKE_GENERATOR_PLATFORM=x64 .
+REM cmake -DBUILD_SHARED_LIBS=1 -G "Visual Studio 14 2015 Win64" .
 msbuild GLFW.sln
 cd ../..
 node-gyp rebuild
