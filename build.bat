82823bcc2315eef72318dc2b5318f0b1d9ca52d2
Kenny Yuan <kaining.yuan@intel.com>
2017-08-25 16:05:49 +0800

    Make it build on clean windows repo

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ echo %0 %1 %2
 cd %2
 cd deps
 cd glfw-3.0.4
-del CMakeCache.txt
+if exist CMakeCache.txt del /f /q CMakeCache.txt
 
 IF /I "%1"=="x64" (
 	cmake -DBUILD_SHARED_LIBS=1 -DGLFW_BUILD_EXAMPLES=0 -DGLFW_BUILD_TESTS=0 -DGLFW_BUILD_DOCS=0 -DCMAKE_GENERATOR_PLATFORM=x64 . > NUL
